8c813eac65715141c218e708246334b16c021272
RomainPALAYRET <117434230+RomainPALAYRET@users.noreply.github.com>
2023-02-26 23:50:55 +0100

    Test 3 : echec

--- TestFibo.java
+++ TestFibo.java
@@ -20,7 +20,6 @@ public class TestFibonacci {
      *  Test n°1
      *  FiboIterator.hasNext() renvoie true car la borne max est à 5
      */
-
     @Test
     public void Test1() {
         assertEquals(true, (new FiboIterator(5)).hasNext());
@@ -30,9 +29,20 @@ public class TestFibonacci {
      *  Test n°2
      *  FiboIterator.hasNext() renvoie false car la borne max est à 0
      */
-
     @Test
     public void Test2() {
         assertEquals(false, (new FiboIterator(0)).hasNext());
+    }
+
+    /**
+     *  Test n°3
+     *  FiboIterator.hasNext() renvoie false car on atteind la borne après un next()
+     *
+     */
+    @Test
+    public void Test3() {
+        FiboIterator it = new FiboIterator(1);
+        it.next();
+        assertEquals(false, it.hasNext());
     }
 }
